29c9b41c7f09e5af7fe7b99ade112cc786d88de9
nguyenr95 <43419662+nguyenr95@users.noreply.github.com>
2023-09-05 21:32:11 -0400

    Delete preprocess directory

--- preprocess/bhv2_to_bhv_RE.m
+++
@@ -1,5 +0,0 @@
-function ml = bhv2_to_bhv_RE(MLdata, TrialRecord) %data and TrialRecord are variables from mlconcatenate
-ml.ConditionNumber = MLdata.Condition; 
-ml.TrialError = MLdata.TrialError; 
-ml.TimingFileByCond = TrialRecord.TaskInfo.TimingFileByCond; 
-end 
--- preprocess/save_pupil_movie.m
+++
@@ -1,8 +0,0 @@
-%load movie
-v = VideoReader('NN11_210705_001_eye.mj2');
-frames = read(v,[1 Inf]);
-%% save movie
-m = VideoWriter('pupil.avi');
-open(m)
-writeVideo(m,frames)
-close(m)
